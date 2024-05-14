2341648924799aefc304f43a343ef41789d49f1a
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2024-05-14 16:49:06 +0200

    continued work

--- app_pred_maladie_retine.py
+++ app_pred_maladie_retine.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/index/")
+def index():
+    return render_template("index.html")

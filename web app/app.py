14d57e56bc7431e95f95f067de2b5d33578e1a89
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2024-06-01 17:49:30 +0200

    continued work

--- web app/app.py
+++ web app/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SECRET_KEY"] = "42isTheAnswer"  # Nécessaire pour les sessions
 db = SQLAlchemy(app)
+
 
 # Définition du modèle de la base de données
 class User(db.Model):
@@ -11,32 +13,41 @@ class User(db.Model):
     username = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+
+# Création de la base de données si elle n'existe pas
+with app.app_context():
+    db.create_all()
+
+
 # Page d'accueil
 @app.route("/")
 def home():
-    if 'username' in request.cookies:
+    if "username" in session:
         return render_template("home.html")
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
+
 
 # Page de connexion
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        username = request.form.get('userId')
-        password = request.form.get('password')
+        username = request.form.get("userId")
+        password = request.form.get("password")
         user = User.query.filter_by(username=username, password=password).first()
         if user:
+            session["username"] = user.username
             return redirect(url_for("home"))
     return render_template("login.html")
+
 
 # Page d'inscription
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        username = request.form.get('userId')
-        password = request.form.get('password')
-        confirm_password = request.form.get('confirmPassword')
+        username = request.form.get("userId")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirmPassword")
         if password == confirm_password:
             new_user = User(username=username, password=password)
             db.session.add(new_user)
@@ -44,6 +55,13 @@ def register():
             return redirect(url_for("login"))
     return render_template("register.html")
 
+
+# Déconnexion
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return redirect(url_for("login"))
+
+
 if __name__ == "__main__":
-    db.create_all()
     app.run(debug=True)

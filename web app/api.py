f2157358c47312cab5376a83d9ba56176d61b18d
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2024-06-02 14:23:42 +0200

    continued work

--- web app/api.py
+++ web app/api.py
@@ -1,4 +1,4 @@
-# Importation des bibliothèques reqquises
+# Importation des bibliothèques requises
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import JSONResponse
 from tensorflow.keras.models import load_model
@@ -7,13 +7,17 @@ import numpy as np
 import uvicorn
 import os
 
-app = FastAPI()
+# Création de l'application FastAPI avec une description et une version
+app = FastAPI(
+    title="Classification des images de la rétine",
+    description="Cette API effectue la classification des images de la rétine pour détecter les maladies oculaires telles que la rétinopathie diabétique.",
+    version="1.0"
+)
 
-# Load the trained models
+# Chargement du modèle entraîné
 vgg16_model = load_model("VGG16-model-classification-maladie-retine.h5")
-# inceptionv3_model = load_model("InceptionV3-model-classification-maladie-retine.h5")
 
-# Route de l'api qui gère la classification des images oct envoyé par la web app
+# Route de l'API qui gère la classification des images oculaires envoyées par la web app
 @app.post("/classify/")
 async def classify_image(file: UploadFile = File(...)):
     contents = await file.read()
@@ -25,7 +29,7 @@ async def classify_image(file: UploadFile = File(...)):
     image = np.expand_dims(image, axis=0)
     image = image / 255.0
 
-    # You can choose which model to use for classification
+    # Vous pouvez choisir quel modèle utiliser pour la classification
     prediction = vgg16_model.predict(image)
     class_idx = np.argmax(prediction[0])
     class_labels = ["Normal", "CNV", "DME", "Drusen"]
